f071cb9183d932535b6a784460ddab0854c8edc6
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-09 19:50:26 +0100

    update name

--- car_rental/src/main/java/com/projecttuto/vehicule_rental/VehiculeRentalApplication.java
+++ car_rental/src/main/java/com/projecttuto/vehicule_rental/VehiculeRentalApplication.java
@@ -1,4 +1,4 @@
-package com.projecttuto.car_rental;
+package com.projecttuto.vehicule_rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
