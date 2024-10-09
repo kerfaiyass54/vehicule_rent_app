dd59bda364f0210e6dc439fa3aff10f7f4d83a86
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-09 18:24:17 +0100

    initialized project

---
+++ car_rental/src/main/java/com/projecttuto/car_rental/VehiculeRentalApplication.java
@@ -0,0 +1,13 @@
+package com.projecttuto.car_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculeRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculeRentalApplication.class, args);
+	}
+
+}
