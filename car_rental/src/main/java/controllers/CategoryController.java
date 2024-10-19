76bb9099de8375bc1cabb6c24521bc72815cd15a
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-19 21:06:33 +0100

    initialized controllers

--- car_rental/src/main/java/controllers/CategoryController.java
+++ car_rental/src/main/java/controllers/CategoryController.java
@@ -1,4 +1,10 @@
 package controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
 }
--- car_rental/src/main/java/controllers/LocationController.java
+++ car_rental/src/main/java/controllers/LocationController.java
@@ -1,4 +1,8 @@
 package controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/location")
 public class LocationController {
 }
