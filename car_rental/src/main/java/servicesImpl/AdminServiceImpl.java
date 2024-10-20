9c1676f143338c4d17ea879580fdf5cb11583b7f
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-20 18:21:19 +0100

    admin's service implementation

--- car_rental/src/main/java/servicesImpl/AdminServiceImpl.java
+++ car_rental/src/main/java/servicesImpl/AdminServiceImpl.java
@@ -1,16 +1,49 @@
 package servicesImpl;
 
 
+import entities.Client;
+import entities.Repair;
+import entities.Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.AdminRepository;
+import repositories.ClientRepository;
+import repositories.RepairRepository;
+import repositories.SupplierRepository;
 import services.AdminService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
 
     @Autowired
     private AdminRepository adminRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private RepairRepository RepairRepository;
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    @Override
+    public List<Client> getClients(){
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public List<Repair> getRepairs(){
+        return RepairRepository.findAll();
+    }
+
+    @Override
+    public List<Supplier> getSuppliers(){
+        return supplierRepository.findAll();
+    }
 
 
 }
