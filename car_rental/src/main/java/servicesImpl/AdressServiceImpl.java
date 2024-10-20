5d926210f4ba854ab9596f87e21f0eebce802f4a
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-20 18:29:57 +0100

    implemented adress service

--- car_rental/src/main/java/servicesImpl/AdressServiceImpl.java
+++ car_rental/src/main/java/servicesImpl/AdressServiceImpl.java
@@ -1,13 +1,61 @@
 package servicesImpl;
 
+import entities.Adress;
+import entities.Location;
+import entities.Supplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.AdressRepository;
+import repositories.LocationRepository;
+import repositories.SupplierRepository;
 import services.AdressService;
+
+import java.util.List;
 
 @Service
 public class AdressServiceImpl implements AdressService {
 
     @Autowired
     private AdressRepository adressRepository;
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Override
+    public List<Adress> getAdresses(){
+        return adressRepository.findAll();
+    }
+
+    @Override
+    public void addAdress(Adress adress){
+        adressRepository.save(adress);
+    }
+
+    @Override
+    public void deleteAdress(Adress adress){
+        adressRepository.delete(adress);
+    }
+
+    @Override
+    public void updateAdress(Adress adress){
+        Adress adr = adressRepository.getById(adress.getIdAdress());
+        adr.setRoad(adress.getRoad());
+        adr.setLocation(adress.getLocation());
+        adr.setNumber(adress.getNumber());
+        adr.setSupplier(adress.getSupplier());
+        adressRepository.save(adr);
+    }
+
+    @Override
+    public Supplier getSupplier(String suppName){
+        return supplierRepository.findByName(suppName);
+    }
+
+    @Override
+    public Location getLocation(String locationName){
+        return locationRepository.findByName(locationName);
+    }
 }
