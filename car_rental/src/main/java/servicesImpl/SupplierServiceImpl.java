5b56bb680a24ebcb4935ac0430cc8a36f99a15fd
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-20 20:35:53 +0100

    implemented supplier service

--- car_rental/src/main/java/servicesImpl/SupplierServiceImpl.java
+++ car_rental/src/main/java/servicesImpl/SupplierServiceImpl.java
@@ -1,10 +1,16 @@
 package servicesImpl;
 
 
+import entities.Adress;
+import entities.Subscription;
+import entities.Supplier;
+import entities.Vehicule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.*;
 import services.SupplierService;
+
+import java.util.List;
 
 @Service
 public class SupplierServiceImpl implements SupplierService {
@@ -22,5 +28,33 @@ public class SupplierServiceImpl implements SupplierService {
 
     @Autowired
     private DemandRepository demandRepository;
+
+
+    @Override
+    public void addSupplier(Supplier supplier){}
+    @Override
+    public void updateSupplier(Supplier supplier){}
+    @Override
+    public void deleteSupplier(Supplier supplier){}
+    @Override
+    public Supplier getSupplier(long id){
+        return supplierRepository.getById(id);
+    }
+    @Override
+    public void changeSupplierPassword(Supplier supplier, String newPassword){
+
+    }
+    @Override
+    public List<Subscription> getSubscriptions(Supplier supplier){
+        return supplierRepository.getById(supplier.getIdSupp()).getSubscriptions();
+    }
+    @Override
+    public List<Adress> getAdresses(Supplier supplier){
+        return supplierRepository.getById(supplier.getIdSupp()).getAdresses();
+    }
+    @Override
+    public List<Vehicule> getVehicules(Supplier supplier){
+        return supplierRepository.getById(supplier.getIdSupp()).getVehicules();
+    }
 
 }
