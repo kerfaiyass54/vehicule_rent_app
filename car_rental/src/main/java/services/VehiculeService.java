ae5ab73cc12d07a74dd79c958fa6f1858a43bd2e
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-25 10:57:08 +0100

    Added methods to the vehicule service

--- car_rental/src/main/java/services/VehiculeService.java
+++ car_rental/src/main/java/services/VehiculeService.java
@@ -2,13 +2,19 @@ package services;
 
 import entities.Category;
 import entities.Vehicule;
+import enums.CategoryName;
 
 public interface VehiculeService {
 
-    void addVehicule(Vehicule vehicule);
+    void addVehicule(Vehicule vehicule, String nameSupplier, CategoryName nameCategory);
     void deleteVehicule(Vehicule vehicule);
     void updateVehicule(Vehicule vehicule);
     Vehicule getVehicule(long id);
     Category getCategory(long id);
+    void buyVehicule(String vehiculeName, String nameClient, int period);
+    boolean canHeBuyTheVehicule(Vehicule vehicule);
+    void returnVehicule(String vehiculeName, String nameClient);
+
+
 
 }
