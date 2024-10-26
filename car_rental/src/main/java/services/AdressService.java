904e205a3b64af8f9b84a4cc7634958954b9252e
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-26 20:48:57 +0100

    Updated adress methods

--- car_rental/src/main/java/services/AdressService.java
+++ car_rental/src/main/java/services/AdressService.java
@@ -9,9 +9,10 @@ import java.util.List;
 public interface AdressService {
     List<Adress> getAdresses();
     void addAdress(Adress adress);
-    void deleteAdress(Adress adress);
-    void updateAdress(Adress adress);
+    void deleteAdress(String adressName);
+    void updateAdress(String adressName);
     Supplier getSupplier(String suppName);
     Location getLocation(String locationName);
+    void freeAdress(String adressName);
 
 }
