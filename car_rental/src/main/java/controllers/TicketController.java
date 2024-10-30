0a4aa555076c166b2da8a5dc62476fb2f1d3f39c
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-30 12:05:03 +0100

    Added ticket controller

--- car_rental/src/main/java/controllers/TicketController.java
+++ car_rental/src/main/java/controllers/TicketController.java
@@ -1,10 +1,11 @@
 package controllers;
 
 
+import entities.Client;
+import entities.Repair;
+import entities.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import services.TicketService;
 
 @RestController
@@ -14,4 +15,27 @@ public class TicketController {
 
     @Autowired
     private TicketService ticketService;
+
+    @GetMapping("/client/{name}")
+    public Client getClient(@PathVariable String name){
+        return ticketService.getClient(name);
+    }
+
+    @GetMapping("/repair/{name}")
+    public Repair getRepair(@PathVariable String name){
+        return ticketService.getRepair(name);
+    }
+
+    @GetMapping("/status/{id}")
+    public void changeStatus(@PathVariable long id){
+        ticketService.changeStatus(id);
+    }
+
+    @GetMapping("/ticket/{repairName}")
+    public void addTicket(@RequestBody Ticket ticket,@PathVariable String repairName,@RequestParam String clientName){
+        ticketService.addTicket(ticket, repairName, clientName);
+    }
+
+    @GetMapping("/remove")
+    public void removeTicket(@RequestBody Ticket ticket){}
 }
