bd1a57497bcb7a157494acab6185d225fd8f960e
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-10-30 11:17:23 +0100

    Added admin controller

--- car_rental/src/main/java/controllers/AdminController.java
+++ car_rental/src/main/java/controllers/AdminController.java
@@ -1,11 +1,12 @@
 package controllers;
 
 
+import entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import services.AdminService;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -14,4 +15,42 @@ public class AdminController {
 
     @Autowired
     private AdminService adminService;
+
+    @GetMapping("/clients")
+    public List<Client> getClients(){
+        return adminService.getClients();
+    }
+
+    @GetMapping("/suppliers")
+    public List<Supplier> getSuppliers(){
+        return adminService.getSuppliers();
+    }
+
+    @GetMapping("/repairs")
+    public List<Repair> getRepairs(){
+        return adminService.getRepairs();
+    }
+
+    @GetMapping("/repairs")
+    List<Location> getLocations(){
+        return adminService.getLocations();
+    }
+
+
+    @PutMapping("/update-admin")
+    void updateDetails(@RequestBody Admin admin){
+        adminService.updateDetails(admin);
+    }
+
+
+    @GetMapping("/admin-details/{adminName}")
+    Admin getDetails(@PathVariable String adminName){
+        return adminService.getDetails(adminName);
+    }
+
+
+
+
+
+
 }
