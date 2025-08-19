395ca72ede4f0c11f4466d2dd7e429c76f9acc2a
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-08-19 22:10:23 +0100

    Cleared controller code

--- car_rental/src/main/java/com/projecttuto/vehicule_rental/controllers/SubscriptionController.java
+++ car_rental/src/main/java/com/projecttuto/vehicule_rental/controllers/SubscriptionController.java
@@ -2,7 +2,19 @@ package com.projecttuto.vehicule_rental.controllers;
 import com.projecttuto.vehicule_rental.DTO.SubscriptionDTO;
 import com.projecttuto.vehicule_rental.entities.Subscription;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;;
 import com.projecttuto.vehicule_rental.services.SubscriptionService;
 
 @RestController
