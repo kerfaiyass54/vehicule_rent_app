16c0d11dbf84e67d901f34d23ec490ad31a478d9
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2025-08-25 22:03:20 +0100

    Fixed keycloak issues

---
+++ car_rental/src/main/java/com/projecttuto/vehicule_rental/configuration/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.projecttuto.vehicule_rental.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").hasRole("admin")
+                        .requestMatchers("/client/**").hasRole("client")
+                        .requestMatchers("/supplier/**").hasRole("supplier")
+                        .requestMatchers("/repairer/**").hasRole("repairer")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+        return http.build();
+    }
+}
