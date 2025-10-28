e1eb7145fc690fb3c2a01709446ba8b5592f0fc6
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-10-28 21:45:25 +0100

    Added swagger

---
+++ car_rental/src/main/java/com/projecttuto/vehicule_rental/configuration/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.projecttuto.vehicule_rental.configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("This is my API documentation"));
+    }
+}
