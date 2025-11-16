da09537a6796e4eb6a2b9e64206979c0cd7ec3ef
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-16 16:04:53 +0100

    Updated controllers by giving responseentty as type

--- car_rental/src/main/java/com/projecttuto/vehicule_rental/controllers/KeycloakController.java
+++ car_rental/src/main/java/com/projecttuto/vehicule_rental/controllers/KeycloakController.java
@@ -2,9 +2,10 @@ package com.projecttuto.vehicule_rental.controllers;
 
 
 import com.projecttuto.vehicule_rental.services.KeycloakAdminService;
-
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,37 +23,47 @@ public class KeycloakController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestParam String username,
-                             @RequestParam String email,
-                             @RequestParam String password,
-                             @RequestParam String role) {
+    public ResponseEntity<Void> createUser(@RequestParam String username,
+                                     @RequestParam String email,
+                                     @RequestParam String password,
+                                     @RequestParam String role) {
         keycloakService.createUser(username, email, password, role);
-        return "✅ User created successfully!";
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/users")
-    public List<UserRepresentation> getUsers() {
-        return keycloakService.getAllUsers();
+    public ResponseEntity<List<UserRepresentation>> getUsers() {
+        List<UserRepresentation> users = keycloakService.getAllUsers();
+        if(users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(users);
     }
 
     @DeleteMapping("/users/{id}")
-    public String deleteUser(@PathVariable String id) {
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
         keycloakService.deleteUser(id);
-        return "🗑️ User deleted!";
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/users/{id}/password")
-    public String updatePassword(@PathVariable String id, @RequestParam String newPassword) {
+    public ResponseEntity<Void> updatePassword(@PathVariable String id, @RequestParam String newPassword) {
         keycloakService.updatePassword(id, newPassword);
-        return "🔑 Password updated!";
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 
 
 
     @GetMapping("/roles")
-    public List<RoleRepresentation> getRoles() {
-        return keycloakService.getAllRoles();
+    public ResponseEntity<List<RoleRepresentation>> getRoles() {
+        List<RoleRepresentation> roles = keycloakService.getAllRoles();
+        if(roles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(roles);
     }
+
+
 
 
 }
