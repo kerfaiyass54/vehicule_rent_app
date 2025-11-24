c7839eca5acb158f0b6f988fc125a76892d39a59
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-24 19:44:33 +0100

    Added first unit tests with their config

---
+++ car_rental/src/test/java/com/projecttuto/car_rental/repositories/AdminRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.projecttuto.car_rental.repositories;
+
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class AdminRepositoryTest {
+
+}
