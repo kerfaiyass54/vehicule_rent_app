b9333503146bd61d66922076b35df7ae6bc3e4ec
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-09 16:55:22 +0100

    Fixed cros origin

--- car_rental/src/main/java/com/projecttuto/vehicule_rental/controllers/UserLoginSessionController.java
+++ car_rental/src/main/java/com/projecttuto/vehicule_rental/controllers/UserLoginSessionController.java
@@ -6,12 +6,7 @@ import com.projecttuto.vehicule_rental.entities.UserLoginSession;
 import com.projecttuto.vehicule_rental.services.UserLoginSessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/sessions")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class UserLoginSessionController {
 
     private final UserLoginSessionService service;
