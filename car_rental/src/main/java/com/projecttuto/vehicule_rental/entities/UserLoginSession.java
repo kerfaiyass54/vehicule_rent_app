a79c1799ff056b03f29d4a520f4146692ed0520a
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-14 22:08:29 +0100

    Added session entity*

--- car_rental/src/main/java/com/projecttuto/vehicule_rental/entities/UserLoginSession.java
+++ car_rental/src/main/java/com/projecttuto/vehicule_rental/entities/UserLoginSession.java
@@ -1,20 +1,25 @@
 package com.projecttuto.vehicule_rental.entities;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.Instant;
 
 @Data
+@Getter
+@Setter
+@NoArgsConstructor
 @Document(indexName = "user_login_sessions")
 public class UserLoginSession {
 
     @Id
     private String id;
+    private String userId;
     private String username;
     private String email;
-    private LocalDate loginDate;
-    private LocalTime loginTime;
+    private Instant sessionStart;
 }
